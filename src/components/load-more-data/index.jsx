112ed6ac5f7cd448f2534fa6f9d720ea796e037c
Divyansh Savita <savitadivyansh323@gmail.com>
2026-01-16 00:15:42 +0530

    functionality added

---
+++ src/components/load-more-data/index.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+export default function LoadMoreData(){
+    const [Loading,setLoading]=useState(false);
+    const [products,setproducts]=useState([]);
+    const [count,setcount]=useState(0);
+    async function fetchProducts() {
+        try {
+            setLoading(true)
+            const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0:count}`)
+            const result = await response.json();
+            
+            if(result && result.products && result.products.length){
+                setproducts((prevData)=>[...prevData,...result.products]);
+                setLoading(false)
+            }
+            
+        } catch (error) {
+            console.log(error);
+            setLoading(false)            
+        }
+    }
+    useEffect(()=>{
+        fetchProducts()
+    },[count])
+    if(Loading){
+        return <div>Loading data ! Please wait.</div>
+    }
+return <div className="container">
+    <div>
+        {
+            products && products.length ? products.map(item =>(
+            <div className="product" key={item.id}>
+            <img src={item.thumbnail} alt={item.title}/> </div>))
+            :null 
+                
+        }
+    </div>
+    <button onClick={()=>setcount(count+1)}>Load More Products</button>
+</div>
+}
