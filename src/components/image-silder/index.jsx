f27173084169791204421c8a96d7213505b1e7b9
Divyansh Savita <savitadivyansh323@gmail.com>
2026-01-19 22:38:18 +0530

    new project added

---
+++ src/components/image-silder/index.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react"
+
+
+export default function ImageSlider({url,limit}){
+
+    const [images,setImages] = useState([]);
+    const [currentSlide,setCurrentSlide] = useState(0);
+    const [errorMsg,setErrorMsg] = useState(null);
+    const [loading,setLoading] = useState(false);
+    async function fetchImages(getUrl) {
+        try{
+            setLoading(true)
+            const response = await fetch(getUrl);
+            const data = await response.json();
+            if(data){
+                setImages(data);
+            }
+        }catch(e){
+            setErrorMsg(e.nessage);
+            setLoading(false);
+        }
+    }
+
+    useEffect(()=>{
+        if(url !== "")fetchImages(url);
+    },[url]);
+
+    if(loading){
+        return <div>Loading data!Please wait</div>
+    }
+    if(errorMsg !== null){
+        return <div>Error Occured! {errorMsg}</div>
+    }
+    return <div className="container">
+
+    </div>
+}
